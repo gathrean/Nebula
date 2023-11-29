09a1e4e44fddea4459f557dfc2d7054eaf09e5f8
Bardia <bilbeigitimouri@my.bcit.ca>
2023-11-29 14:05:43 -0800

    Fixed parsing of results.

--- frontend/index.js
+++ frontend/index.js
@@ -43,12 +43,32 @@ function handleFileUpload(file) {
     });
 }
 
-function displayPredictedLabels(labels) {
+function displayPredictedLabels(labelsData) {
     const labelsContainer = document.getElementById('predictedLabels');
     labelsContainer.innerHTML = '';
-    labels.forEach(label => {
+
+    let labelsString = '';
+    if (Array.isArray(labelsData)) {
+        labelsString = labelsData.join(',');
+    } else if (typeof labelsData === 'string') {
+        labelsString = labelsData;
+    } else {
+        console.error('Invalid labels data type:', labelsData);
+        return;
+    }
+
+    const labelParts = labelsString.split(',').filter(part => part.includes(':'));
+
+    // Skip the first and the last elements of the array
+    for (let i = 1; i < labelParts.length - 1; i++) {
+        const part = labelParts[i];
+        const [instrument, probability] = part.split(':').map(s => s.trim());
+        const probabilityPercentage = (parseFloat(probability) * 100).toFixed(2) + '%';
         const labelElement = document.createElement('div');
-        labelElement.textContent = label;
+        labelElement.textContent = `${instrument}: ${probabilityPercentage}`;
         labelsContainer.appendChild(labelElement);
-    });
+    }
 }
+
+
+
